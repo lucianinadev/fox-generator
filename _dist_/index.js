d052056bd7be745436634d546faf7802b659dc7d
lucianinadev <lucianinadev@users.noreply.github.com>
2022-04-24 22:13:47 +0000

    deploy: af9952b833dc4762f2c249d6a969b9e87715f955

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,42 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+import { registerImage } from './lazy.js'
+
+const min = 1;
+const max = 123;
+
+const app = document.querySelector("#app");
+
+const random = () => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const createImageNode = () => {
+    const container = document.createElement("div");
+    container.className = "p-4";
+
+    const imagen = document.createElement("img");
+    imagen.width = "250";
+    imagen.alt = `fox-${random(min, max)}`;
+    imagen.className = "mx-auto rounded preloader";
+    imagen.dataset.src = `https://randomfox.ca/images/${random(min, max)}.jpg`;
+
+    container.appendChild(imagen);
+    return container;
+};
+
+const btnLoad = document.querySelector("#btn-load");
+
+const addImage = () => {
+    const newImage = createImageNode();
+    app.append(newImage);
+    registerImage(newImage)
+    
+};
+btnLoad.addEventListener("click", addImage);
+
+const btnClean = document.querySelector("#btn-clean");
+
+const limpiar = () => {
+    app.innerHTML = ''
+};
+btnClean.addEventListener("click", limpiar);
